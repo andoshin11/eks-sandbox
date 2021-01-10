e30c2dc81c47e626943b1481c963a13695258461
shin-ando <shin.ando@linecorp.com>
2021-01-11 01:19:00 +0900

    debug

--- terraform/modules/gw.tf
+++ terraform/modules/gw.tf
@@ -1,5 +1,5 @@
-resource "aws_internet_gateway" "main" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
-  tags = merge(local.default_tags, map("Name", "${local.base_name}-main"))
+  tags = merge(local.default_tags, map("Name", "${local.base_name}-igw"))
 }
